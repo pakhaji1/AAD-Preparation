c07880f8a3ab8b219a2a41652c2b0bedef330d7e
Muhammad Rizeky Rahmatullah <128965679+pakhaji1@users.noreply.github.com>
2023-07-11 20:35:45 +0800

    Add files via upload

---
+++ Habit App/HabitApp/app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -0,0 +1,33 @@
+package com.dicoding.habitapp.ui.list
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import com.dicoding.habitapp.R
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HabitActivityTest {
+
+    @Before
+    fun setUp(){
+        ActivityScenario.launch(HabitListActivity::class.java)
+        Intents.init()
+    }
+
+    @Test
+    fun openAddHabitActivity(){
+        onView(ViewMatchers.withId(R.id.fab)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(AddHabitActivity::class.java.name))
+    }
+}
